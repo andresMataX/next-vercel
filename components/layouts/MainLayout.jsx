5e36746ffad3ee419794d73684a33ecc24119658
Jesús Mata <andresmata.c21@gmail.com>
2023-05-09 21:22:24 -0600

    Add MainLayout

---
+++ components/layouts/MainLayout.jsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from './MainLayout.module.css'
+import { NavBar } from '../NavBar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function MainLayout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>About - Andrés</title>
+        <meta name='description' content='Home page' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <NavBar />
+
+      <main className={`${styles.main} ${inter.className}`}>{children}</main>
+    </>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,18 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import { NavBar } from '@/components/NavBar'
-
-const inter = Inter({ subsets: ['latin'] })
+import MainLayout from '@/components/layouts/MainLayout'
 
 export default function HomePage() {
   return (
-    <>
-      <Head>
-        <title>About - Andrés</title>
-        <meta name='description' content='Home page' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <MainLayout>
+      <div className={'description'}>
+        <h1>Home Page</h1>
+      </div>
 
-      <NavBar />
-
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <h1>Home Page</h1>
-        </div>
-
-        <div className={styles.grid}>
-          <Link className={styles.card} href='/about'>
-            Ir a About
-          </Link>
-        </div>
-      </main>
-    </>
+      <div className={'grid'}>
+        <Link className={'card'} href='/about'>
+          Ir a About
+        </Link>
+      </div>
+    </MainLayout>
   )
 }
